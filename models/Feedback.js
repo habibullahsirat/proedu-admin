b8f30787ef056cdd1b12bd1d6d3ff0e7cb0f232b
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-06-30 22:06:12 +0600

    Feedback model created.

---
+++ models/Feedback.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const FeedbackSchema = new mongoose.Schema(
+  {
+    feedback: {
+      type: String,
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+
+    image: String,
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.models.Feedback ||
+  mongoose.model("Feedback", FeedbackSchema);
---
+++ models/SuccessfulStudent.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const SuccessfulStudentSchema = new mongoose.Schema(
+  {
+    image: String,
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.models.SuccessfulStudent ||
+  mongoose.model("SuccessfulStudent", SuccessfulStudentSchema);
