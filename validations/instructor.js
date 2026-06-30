c8e3d800346ca65439660bbc31ef0b4c7d4e2a27
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-01 01:56:10 +0600

    Instructor validation created.

---
+++ validations/instructor.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const instructorSchema = z.object({
+  image: z.object({
+    url: z.string().url(),
+    public_id: z.string(),
+  }),
+
+  name: z.string().min(2),
+
+  email: z.string().email(),
+
+  department: z.string().min(2),
+
+  details: z.string().min(20),
+});
