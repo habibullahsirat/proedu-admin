35e67ab4c435d6f32458419dd168cb372e5049a8
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-01 01:57:49 +0600

    Feedback validation created.

---
+++ validations/feedback.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const feedbackSchema = z.object({
+  feedback: z.string().min(10),
+
+  rating: z.number().min(1).max(5),
+
+  image: z.object({
+    url: z.string().url(),
+    public_id: z.string(),
+  }),
+
+  name: z.string().min(2),
+
+  department: z.string().min(2),
+});
